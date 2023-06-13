44a7e19388608d7f3a5bf476a9f4c0c623bbf5c5
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 12:39:12 +0530

    Math class

---
+++ src/MathClass.java
@@ -0,0 +1,18 @@
+public class MathClass {
+
+    public static void main(String args[]){
+
+        System.out.println("Max of 5 and 10 is : "+Math.max(5,10));
+
+        System.out.println("Min of 5 and 10 is : "+Math.min(5,10));
+
+        System.out.println("Ceil of 10.4 is : "+Math.ceil(10.4));
+
+        System.out.println("Floor of 10.4 is : "+Math.floor(10.4));
+
+        System.out.println("Sqrt of 16 is : "+Math.sqrt(16));
+
+        System.out.println("Abs of -10 is : "+Math.abs(-10));
+
+    }
+}

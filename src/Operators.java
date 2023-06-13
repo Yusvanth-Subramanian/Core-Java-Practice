567ba131671023b5911fcbe6c77098062cf066ae
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 14:15:12 +0530

    Operators

---
+++ src/Operators.java
@@ -0,0 +1,17 @@
+public class Operators {
+
+    public static void main(String args[]){
+
+        if(5==5)
+            System.out.print("Equal to operator.");
+        if(5!=6)
+            System.out.print("Not equal to operator.");
+        boolean b1 = true;
+        boolean b2 =false;
+        if(b1&&b2)
+            System.out.print("Both are true");
+        if(b1||b2)
+            System.out.print("Any one or both are true");
+
+    }
+}

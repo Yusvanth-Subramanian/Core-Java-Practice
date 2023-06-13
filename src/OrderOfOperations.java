0bc24f63f8e1a0c7c60929577ff7258f735ed889
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 12:15:19 +0530

    Order of Operations

---
+++ src/OrderOfOperations.java
@@ -0,0 +1,14 @@
+public class OrderOfOperations {
+
+    public static void main(String args[]){
+
+        int a = 10 + 3 * 2;
+        System.out.println("10 + 3 * 2 = "+a);
+
+        a=(10+3)*2;
+        System.out.println("( 10 + 3 ) * 2 = "+a);
+
+        a=10^3*2;
+        System.out.println("10 ^ 3 * 2 = "+a);
+    }
+}

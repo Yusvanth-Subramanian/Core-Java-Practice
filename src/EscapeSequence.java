c7c9c52090416bf4434c2aa894c855681147de6f
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 11:43:01 +0530

    Escape Sequence

---
+++ src/EscapeSequence.java
@@ -0,0 +1,13 @@
+public class EscapeSequence {
+
+    public static void main(String args[]){
+
+        System.out.println("Adding double quotations : \"");
+
+        System.out.println("Adding single quotations : \'");
+
+        System.out.println("Giving 4 spaces using tab :\tThis is after using \\t");
+
+        System.out.println("Moving to the next line : \nThis is after using \\n");
+    }
+}

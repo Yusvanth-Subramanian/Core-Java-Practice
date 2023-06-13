9730d710f28db191e26c3a4baa9d4b0cf10412aa
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 12:09:56 +0530

    Constants

---
+++ src/Constants.java
@@ -0,0 +1,11 @@
+public class Constants {
+
+    public static void main(String args[]){
+
+        final int i = 5;
+
+        System.out.println("Final variables cannot be changed.");
+
+        i=6;
+    }
+}

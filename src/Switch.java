51625ce199bb2efd58e62fa9679e47c3e587bfdc
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 14:21:35 +0530

    Switch

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+public class Switch {
+
+    public static void main(String args[]){
+
+        int i=2;
+
+        switch (i){
+            case 1:
+                System.out.print(1);
+                break;
+            case 2:
+                System.out.print(2);
+                break;
+            case 3:
+                System.out.print(3);
+                break;
+            default:
+                System.out.print("Default");
+        }
+
+    }
+}

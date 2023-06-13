285898a359d8e2629288802e0b9ee6e284023c36
Yusvanth Subramanian <yusvanth@terzocloud.com>
2023-06-13 12:29:31 +0530

    Casting

---
+++ src/Casting.java
@@ -0,0 +1,28 @@
+public class Casting {
+
+    public static void main(String args[]){
+
+        short s = 1;
+
+        // short to int
+        int i=s;
+
+        i=2;
+
+        // int to short
+        s = (short) i;
+
+        // float to double
+        double d = 10.5f;
+
+        // double to float
+        float f;
+        f=(float) d;
+
+        // int to long
+        long l = i;
+
+        // long to int
+        i=(int) l;
+    }
+}
